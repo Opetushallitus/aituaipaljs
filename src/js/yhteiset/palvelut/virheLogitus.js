99ba90dab4d59b40adb99dabc2edbe30024caa04
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2014-08-29 15:32:14 +0300

    Hanskaa case jossa window.ophBaseUrl ei ole asetettu.

--- src/js/yhteiset/palvelut/virheLogitus.js
+++ src/js/yhteiset/palvelut/virheLogitus.js
@@ -27,6 +27,8 @@ angular.module('yhteiset.palvelut.virheLogitus', ['ngCookies'])
           var virheviesti = poikkeus.toString();
           var stackTrace = printStackTrace({ e: poikkeus });
 
+          var ophBaseUrl = (window.ophBaseUrl !== undefined ? window.ophBaseUrl : "");
+
           $.ajax({
             type: 'POST',
             url: ophBaseUrl + '/api/jslog/virhe',
